dda5003a6be470bd8342466372003298558f741f
Mezbahul-241-15-929 <islam241-15-929@diu.edu.bd>
2025-07-10 03:23:43 +0600

    49-5 Display user info and sign out

--- Module 49 Simple React Firebase authentication/Module-49-Simple-React-Firebase-authentication/src/Components/Login/Login.jsx
+++ Module 49 Simple React Firebase authentication/Module-49-Simple-React-Firebase-authentication/src/Components/Login/Login.jsx
@@ -1,8 +1,10 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import React from 'react';
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import React, { useState } from 'react';
 import { auth } from "../../firebase/firebase.init";
 
 const Login = () => {
+
+    const [user,setUser]= useState(null);
 
     const provider = new GoogleAuthProvider();
 
@@ -11,9 +13,19 @@ const Login = () => {
 
         signInWithPopup(auth,provider)
         .then(result=>{
-            console.log(result)
+            console.log(result.user);
+            setUser(result.user);
         })
         .catch(error=>{
+            console.log(error)
+        })
+    }
+
+    const handleSignOut = ()=>{
+        signOut(auth).then(()=>{
+            console.log("Signout completed");
+            setUser(null);
+        }).catch(error=>{
             console.log(error)
         })
     }
@@ -21,7 +33,20 @@ const Login = () => {
     return (
         <div>
             <h2>Please Login</h2>
-            <button onClick={handleGoogleSignIn}>Sign in with Google</button>
+            {/* <button onClick={handleGoogleSignIn}>Sign in with Google</button>
+            <button onClick={handleSignOut}>SignOut</button> */}
+
+            {
+                user ? <button onClick={handleSignOut}>SignOut</button>: 
+                <button onClick={handleGoogleSignIn}>Sign in with Google</button>
+            }
+            {
+                user && <div>
+                <h3>{user.displayName}</h3>
+                <h3>{user.email}</h3>
+                <img src={user.photoURL} alt="" />
+            </div>
+            }
         </div>
     );
 };
