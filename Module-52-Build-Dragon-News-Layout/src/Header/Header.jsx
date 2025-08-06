bd0da117273395aa173f2c9ea38f1077e88477d0
Mezbahul-241-15-929 <islam241-15-929@diu.edu.bd>
2025-08-06 20:33:04 +0600

    52-3 Theme Customization, Font & Date-FNS Integration

--- Module-52-Build-Dragon-News-Layout/src/Header/Header.jsx
+++ Module-52-Build-Dragon-News-Layout/src/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from "../assets/logo.png";
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
         <div className='flex justify-center flex-col items-center p-4 gap-3'>
-            <img className='w-[350px]' src={logo} alt="" />
-            <h2>Journalism Without Fear or Favour</h2>
+            <img className='w-[400px]' src={logo} alt="" />
+            <h2 className='text-accent'>Journalism Without Fear or Favour</h2>
             <h3>Sunday, November 27, 2025</h3>
+            <h3>{format(new Date(),"EEEE, MMMM MM, yyyy")}</h3>
         </div>
     );
 };
