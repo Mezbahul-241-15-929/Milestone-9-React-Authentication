9ed21426f0625a547d85a93f178c6e78644747ad
Mezbahul-241-15-929 <islam241-15-929@diu.edu.bd>
2025-09-03 21:39:54 +0600

    52_5_4 Register user & OnAuthStateChange

--- Module-52-Build-Dragon-News-Layout/src/Components/AuthLayout/Register.jsx
+++ Module-52-Build-Dragon-News-Layout/src/Components/AuthLayout/Register.jsx
@@ -1,29 +1,52 @@
-import React from 'react';
+import React, { use } from 'react';
 import { Link } from 'react-router';
+import { AuthContext } from '../../provider/AuthProvider';
 
 const Register = () => {
+    const {createUser,setUser } = use(AuthContext);
+    const handleRegister=(e)=>{
+        e.preventDefault();
+        const form =e.target;
+        const name=form.name.value;
+        const email=form.email.value;
+        const password=form.password.value;
+        const photourl=form.photourl.value;
+        console.log({name,email,password,photourl})
+        createUser(email,password).then((result)=>{
+            const user=result.user;
+            //console.log(user);
+            setUser(user);
+        }).catch((error)=>{
+            //const errorCode =error.code;
+            const errorMessage= error.Message;
+            alert(errorMessage);
+        })
+    }
     return (
         <div className='flex justify-center items-center '>
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                <div className="card-body">
+                <form onSubmit={handleRegister} className="card-body">
                     <h2 className="font-semibold text-2xl text-center">Register your account</h2>
                     <fieldset className="fieldset">
                         {/* Name */}
                         <label className="label">Name</label>
-                        <input type="text" className="input" placeholder="Name" />
+                        <input name='name' type="text" className="input" placeholder="Name" required/>
+                        {/* Photo url */}
+                        <label className="label">Photo url</label>
+                        <input name='photourl' type="text" className="input" placeholder="Photo url" required/>
                         {/* Email */}
                         <label className="label">Email</label>
-                        <input type="email" className="input" placeholder="Email" />
+                        <input name='email' type="email" className="input" placeholder="Email" required/>
                         {/* Password */}
                         <label className="label">Password</label>
-                        <input type="password" className="input" placeholder="Password" />
+                        <input name='password' type="password" className="input" placeholder="Password" required/>
 
-                        <button className="btn btn-neutral mt-4">Register</button>
+                        <button type='submit' className="btn btn-neutral mt-4">Register</button>
                         <p className='font-semibold text-center py-3 text-sm'>Already have an Account ?
                             <Link to="/auth/login" className='text-secondary'> Login</Link>
                         </p>
                     </fieldset>
-                </div>
+                </form>
             </div>
         </div>
     );
--- Module-52-Build-Dragon-News-Layout/src/provider/AuthProvider.jsx
+++ Module-52-Build-Dragon-News-Layout/src/provider/AuthProvider.jsx
@@ -1,16 +1,32 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import app from "../Firebase/firebase.config"
 
 export const AuthContext = createContext();
 
+
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from "firebase/auth";
+const auth = getAuth(app);
+
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    name: "mezbahul",
-    email: "asdf@gmail.com"
-  });
+  const [user, setUser] = useState(null);
+
+  const createUser=(email,password)=>{
+    return createUserWithEmailAndPassword(auth,email,password)
+  };
+
+  useEffect(()=>{
+    const unsubscribe = onAuthStateChanged(auth,(currentUser)=>{
+        setUser(currentUser);
+    });
+    return ()=>{
+        unsubscribe();
+    }
+  },[])
 
   const authData = {
     user,
     setUser,
+    createUser,
   };
 
   return (
